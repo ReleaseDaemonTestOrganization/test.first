8d57afe43037994adcbf270175e14d9531ae3b01
SC-AFI <afi@sitecore.net>
2017-01-22 23:17:11 +0200

    Initial commit

---
+++ src/test.first/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test.first
+{
+    public class Class1
+    {
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
