e3a378f7cc91f40aa0efafaca940bee9f025bfdb
SC-AFI <afi@sitecore.net>
2017-02-09 00:50:58 +0200

    v8.1.1.0

--- src/test.first/Class1.cs
+++ src/test.first/Class1.cs
@@ -7,6 +7,7 @@ namespace test.first
         public void ShowMessage()
         {
             Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+    }
     }
 }
