ea769507e0e933c376ddadf157b499f1a440ae5d
SC-AFI <afi@sitecore.net>
2017-02-13 00:36:38 +0200

    Test: Biuld will failed

--- src/test.first/Class1.cs
+++ src/test.first/Class1.cs
@@ -2,12 +2,12 @@
 
 namespace test.first
 {
-    public class Class1
+  public class Class1
+  {
+    public void ShowMessage()
     {
-        public void ShowMessage()
-        {
-            Console.WriteLine("Hello World!");
       Console.WriteLine("Hello World!");
+      Console.WriteLine("Hello World!")
     }
-    }
+  }
 }
